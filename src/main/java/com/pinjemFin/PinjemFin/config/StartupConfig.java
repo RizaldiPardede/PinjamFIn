7cfdf2b2b54d07bd05671e2833d451654523c79a
RizaldiPardede <rizaldipardede@gmail.com>
2025-05-07 13:47:22 +0700

    startup plafon

--- src/main/java/com/pinjemFin/PinjemFin/config/StartupConfig.java
+++ src/main/java/com/pinjemFin/PinjemFin/config/StartupConfig.java
@@ -1,12 +1,16 @@
 package com.pinjemFin.PinjemFin.config;
 
+import com.pinjemFin.PinjemFin.models.Plafon;
 import com.pinjemFin.PinjemFin.models.Role;
+import com.pinjemFin.PinjemFin.repository.PlafonRepository;
 import com.pinjemFin.PinjemFin.repository.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
 
 @Configuration
@@ -30,5 +34,38 @@ public class StartupConfig {
             }
         };
     }
+
+    @Bean
+    CommandLineRunner initPlafon(PlafonRepository plafonRepository) {
+        return args -> {
+            if (plafonRepository.count() == 0) {
+                Plafon bronze = new Plafon();
+                bronze.setJenis_plafon("Bronze");
+                bronze.setJumlah_plafon(1_000_000.0);
+                bronze.setBunga(5.0);
+
+                Plafon gold = new Plafon();
+                gold.setJenis_plafon("Gold");
+                gold.setJumlah_plafon(25_000_000.0);
+                gold.setBunga(3.0);
+
+                Plafon platinum = new Plafon();
+                platinum.setJenis_plafon("Platinum");
+                platinum.setJumlah_plafon(100_000_000.0);
+                platinum.setBunga(2.0);
+
+                Plafon silver = new Plafon();
+                silver.setJenis_plafon("Silver");
+                silver.setJumlah_plafon(5_000_000.0);
+                silver.setBunga(4.5);
+
+                plafonRepository.saveAll(List.of(bronze, gold, platinum, silver));
+                System.out.println("Inserted default plafon data.");
+            } else {
+                System.out.println("Plafon data already exists.");
+            }
+        };
+    }
+
 
 }
