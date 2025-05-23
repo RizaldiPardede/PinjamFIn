9d677cbbc75b2965ff156388a32022fb64efa70c
RizaldiPardede <rizaldipardede@gmail.com>
2025-05-23 15:57:05 +0700

    buat start config untu feature dan attach ke role super admin 3

--- src/main/java/com/pinjemFin/PinjemFin/repository/FeatureRepository.java
+++ src/main/java/com/pinjemFin/PinjemFin/repository/FeatureRepository.java
@@ -2,6 +2,8 @@ package com.pinjemFin.PinjemFin.repository;
 import com.pinjemFin.PinjemFin.models.Feature;
 import com.pinjemFin.PinjemFin.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,6 @@ import java.util.UUID;
 public interface FeatureRepository extends JpaRepository<Feature, UUID> {
     List<Feature> findAll();
 
-    Feature findByFeatureName(String featureName);
+    @Query("SELECT f FROM Feature f WHERE f.feature_name = :name")
+    Feature findByFeatureName(@Param("name") String name);
 }
